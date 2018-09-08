3419f95d1e72034017745d8fe6cc5757d4b385c5
Niklasalander <niklas.alander@outlook.com>
2018-09-08 14:27:45 +0200

    added a new thread to handle a client whom is playing the game, this client is using another port.

--- UDPproject1A/src/clientside/Client.java
+++ UDPproject1A/src/clientside/Client.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package clientside;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,47 +14,72 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author fno
  */
 public class Client {
-    public static void main(String args[]) throws Exception
-   {
-      BufferedReader inFromUser =
-         new BufferedReader(new InputStreamReader(System.in));
-      DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("localhost");
-      byte[] sendData = new byte[1024];
-      byte[] receiveData = new byte[1024];
-      boolean linkedWithServer = false;
-      if(pokeServer(IPAddress,clientSocket)){
-          String sentence = inFromUser.readLine();
-          while(sentence!=null){
-              sendData = sentence.getBytes();
-              DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-              clientSocket.send(sendPacket);
-              DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-              clientSocket.receive(receivePacket); 
-              String modifiedSentence = new String(receivePacket.getData());
-              System.out.println("FROM SERVER:" + modifiedSentence);
-              sentence = new String(inFromUser.readLine());
-              System.out.println("sentence " +sentence);
-          }
-           clientSocket.close();
-      }
-      
-      
-      
-      
-      
-     
-   }
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader inFromUser
+                = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        int port = 9876;
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        boolean linkedWithServer = false;
+        if (pokeServer(IPAddress, clientSocket)) {
+//            String sentence = inFromUser.readLine();
+            String sentence = "";
+            while (sentence != null) {
+                DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
+                clientSocket.receive(receivePacket);
+                String modifiedSentence = new String(receivePacket.getData());
+                System.out.println("got : " + modifiedSentence);
+                String[] receive = modifiedSentence.trim().split("Q");
+                switch (receive[0]) {
+                    case "BYE":
+                        System.exit(0);
+                    case "USEPORT":
+                        port = Integer.parseInt(receive[1]);
+                        System.out.println("port: " + port);
+                        break;
+                    default:
+                        System.out.println("FROM SERVER:" + modifiedSentence);
+                }
+                
+                sentence = inFromUser.readLine();
+                sendData = new byte[1024];
+                sendData = sentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                System.out.println("port: " + port);
+                clientSocket.send(sendPacket);
+
+
+
+//                sendData = sentence.getBytes();
+//                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+//                clientSocket.send(sendPacket);
+//                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+//                clientSocket.receive(receivePacket);
+//                String modifiedSentence = new String(receivePacket.getData());
+//                System.out.println("FROM SERVER:" + modifiedSentence);
+//                sentence = new String(inFromUser.readLine());
+//                System.out.println("sentence " + sentence);
+            }
+            clientSocket.close();
+        }
+
+    }
+
     /**
-     *This is the protocol that initiates contact with the server if the protocol
-     * is fulfilled then the server accepts the client. if it fails then there is not connection
+     * This is the protocol that initiates contact with the server if the
+     * protocol is fulfilled then the server accepts the client. if it fails
+     * then there is not connection
      */
-    public static boolean pokeServer(InetAddress IPAddress, DatagramSocket clientSocket){
+    public static boolean pokeServer(InetAddress IPAddress, DatagramSocket clientSocket) {
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
         String hello = "HELLO";
@@ -62,7 +88,7 @@ public class Client {
         String ready = "READY";
         String receivedString;
         /**
-         *Send Hello
+         * Send Hello
          */
         sendData = hello.getBytes();
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
@@ -71,19 +97,19 @@ public class Client {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket); // receives ok
             receivedString = new String(receivePacket.getData());
-            System.out.println("[From Server] > " + receivedString );
-            if(!receivedString.trim().equals(ok)){
+            System.out.println("[From Server] > " + receivedString);
+            if (!receivedString.trim().equals(ok)) {
                 System.out.println("not ok");
                 return false;
-            } 
+            }
             sendData = start.getBytes();
             sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
             clientSocket.send(sendPacket); // sends start
             receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
             receivedString = new String(receivePacket.getData());
-            System.out.println("[From Server] > " + receivedString );
-            
+            System.out.println("[From Server] > " + receivedString);
+
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
--- UDPproject1A/src/serverside/Server.java
+++ UDPproject1A/src/serverside/Server.java
@@ -19,54 +19,52 @@ import java.util.logging.Logger;
  */
 public class Server {
 
+    public static final int SERVERPORT = 9876;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          DatagramSocket serverSocket;
+        DatagramSocket serverSocket;
+        Boolean isBusy = false;
         try {
-            serverSocket = new DatagramSocket(9876);
-            
-            
+            serverSocket = new DatagramSocket(SERVERPORT);
+
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
             ServerProtocol serverProtocol = new ServerProtocol();
             System.out.println("Awaiting client activity... ");
-            
-            if(serverProtocol.pokedByClient(serverSocket)){
-                
-                while(true)
-               {
-                   // https://www.baeldung.com/udp-in-java
-                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                  receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                  serverSocket.receive(receivePacket);
-                  //= new String(packet.getData(), 0, packet.getLength());
-                  String sentence = new String( receivePacket.getData(), 0 ,receivePacket.getLength() );
-                  System.out.println("RECEIVED: " + sentence);
-                  InetAddress IPAddress = receivePacket.getAddress();
-                  int port = receivePacket.getPort();
-                  String capitalizedSentence = sentence.toUpperCase();
-                  sendData = capitalizedSentence.getBytes();
-                  DatagramPacket sendPacket =
-                  new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                  serverSocket.send(sendPacket);
-                  
-               }
-                
+
+            if (serverProtocol.pokedByClient(serverSocket, isBusy)) {
+
+                while (true) {
+                    // https://www.baeldung.com/udp-in-java
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    serverSocket.receive(receivePacket);
+                    //= new String(packet.getData(), 0, packet.getLength());
+                    String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    System.out.println("RECEIVED: " + sentence);
+                    InetAddress IPAddress = receivePacket.getAddress();
+                    int port = receivePacket.getPort();
+                    String capitalizedSentence = sentence.toUpperCase();
+                    sendData = capitalizedSentence.getBytes();
+                    DatagramPacket sendPacket
+                            = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                    serverSocket.send(sendPacket);
+
+                }
+
             }
-            
+
             /*
-            */
-            
+             */
         } catch (SocketException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-            
+        }
+
     }
-    
-    
-    
+
 }
